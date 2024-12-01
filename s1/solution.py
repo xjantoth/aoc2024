dee9b8a3d6a73c59600a56e42751e4dd3bf2a99c
xjantoth <toth.janci@gmail.com>
2024-12-01 18:36:06 +0100

    aoc2023 day1 improved

--- s1/solution.py
+++ s1/solution.py
@@ -6,8 +6,8 @@ with open('input.txt', 'r') as f:
     data = f.readlines()
 
 l, r = [], []
-[{l.append(x[0]), r.append(x[1])} for x in [re.findall(r'\d+', i) for i in data]]
+[{l.append(x[0]), r.append(x[1])} for x in [list(map(int, re.findall(r'\d+', i))) for i in data]]
 
-r1 = sum([abs(int(a) - int(b)) for a, b in zip(sorted(l), sorted(r))])
-r2 = sum([int(i)*[int(x) for x in r].count(i) for i in [int(ll) for ll in l]])
+r1 = sum([abs(a - b) for a, b in zip(sorted(l), sorted(r))])
+r2 = sum([i*r.count(i) for i in l])
 print(r1, r2)
